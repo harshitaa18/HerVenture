576a525c56d5324a7c7c1588e0d4d3633d6d6e95
Maitreyee1102 <145358909+Maitreyee1102@users.noreply.github.com>
2025-01-05 11:41:30 +0530

    All pages folder and file structure created

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -13,8 +13,10 @@ export const Navbar = () => {
         </div>
         <ul className='Navbar-menu'>
                 <li><Link to='/' >Home</Link></li>
-                <li><Link to='/aboutus' >About</Link></li>
+                <li><Link to='/aboutUs' >About</Link></li>
                 <li><Link to='/contact' >Contact us</Link></li>
+                <li><Link to='/mentorship'>Mentorship</Link></li>
+                <li><Link to='/faqs'>FAQS</Link></li>
         </ul>
     </div>
   )
