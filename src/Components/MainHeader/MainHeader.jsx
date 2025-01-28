6af5daf1b1562999e02f756e8f38d9c981c52679
harshitaa18 <harshita1874@gmail.com>
2025-01-28 18:00:56 +0530

    .

--- src/Components/MainHeader/MainHeader.jsx
+++ src/Components/MainHeader/MainHeader.jsx
@@ -1,27 +1,12 @@
 import {Link} from 'react-router-dom'
 import Header from '../Assets/new ladki.jpg';
 import "./MainHeader.css";
-
+import post from '../Assets/post.webp'
 const MainHeader = () => {
     return (
-      <header className="main_header">
-        <div className="container main_header-container">
-            <div className="main_header-left">
-            <h4>#Inspiring the world</h4>
-            <h1>Join the legends</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Porro beatae excepturi ea dolor? Soluta, ratione!</p>
-            <Link to="/Mentorship" className='main-header_btn lg'>Get Started</Link>
-            </div>
-
-            <div className="main_header-right">
-                  <div className="main_header-circle"></div>  
-                  <div className="main_header-image">
-                    <img src={Header} alt='' className='circle-img' />
-                  </div>
-                </div>
-        </div>
-      </header>
+      <div className='header'>
+        <img src={post} alt="" />
+      </div>
     )
   }
   
