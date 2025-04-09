0adde862c442464d673143017a73831e34cbff60
kabra-palak <kabrapalak08@gmail.com>
2025-04-10 01:40:57 +0530

    feedback2

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React  from 'react';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import { Link } from 'react-router-dom';
 import { useUser } from '../../Context/UserContext';
 import profilePic from '../Assets/pic3.jpg';
-import Feedback from '../Feedback/Feedback'; // Adjust the path as needed
 
 export const Navbar = () => {
   const { user } = useUser();
-  const [feedbackOpen, setFeedbackOpen] = useState(false); // state to control drawer
   console.log("userData in Navbar:", user);
   return (
     <>
@@ -40,19 +38,8 @@ export const Navbar = () => {
                 <Link to='/login'>LogIn / SignUp</Link>
               </li>
         )}
-
-
-          {/* Feedback Button */}
-          <li>
-            <button onClick={() => setFeedbackOpen(true)} className='feedback-btn'>
-              Feedback
-            </button>
-          </li>
         </ul>
       </div>
-
-      {/* Feedback Drawer */}
-      <Feedback open={feedbackOpen} onToggle={setFeedbackOpen} />
     </>
   );
 };
