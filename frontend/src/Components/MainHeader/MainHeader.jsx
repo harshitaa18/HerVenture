76b37e2113210ff8d83a1b9cedb02825e4fe32af
harshitaa18 <harshita1874@gmail.com>
2025-11-11 19:59:17 +0530

    ...

--- frontend/src/Components/MainHeader/MainHeader.jsx
+++ frontend/src/Components/MainHeader/MainHeader.jsx
@@ -4,17 +4,17 @@ import "./MainHeader.css";
 //import post from '../Assets/post.webp'
 const MainHeader = () => {
   return (
-    <header className="mainpage_header">
-      <div className="mainpage_header-overlay">
-        <div className="container mainpage_header-container">
-          <div className="mainpage_header-text">
+    <header className="mainnpage_header">
+      <div className="mainnpage_header-overlay">
+        <div className="containerr mainnpage_header-container">
+          <div className="mainnpage_header-text">
             <h4>#Inspiring the world</h4>
             <h1>Stronger Together,Bolder Forever</h1>
             <p>
             Empowering women to break barriers, lead boldly, and transform dreams into reality. <br />
              Together, let's build a future where women redefine success on their own terms.
             </p>
-            <Link to="/signup" className="mainpage_header-btn lg">
+            <Link to="/signup" className="mainnpage_header-btn lg">
               Get Started
             </Link>
           </div>
